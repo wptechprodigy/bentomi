c0130e15674cba7075b67db231bb1e6651528cc3
Waheed Afolabi <waheed.remote247@gmail.com>
2022-12-06 18:09:00 +0100

    Basic set up MailKomposer

---
+++ MailKomposer/MailKomposer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MailKomposer
+//
+//  Created by waheedCodes on 06/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
