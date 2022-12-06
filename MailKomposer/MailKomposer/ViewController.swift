f2258b3a29e11ccc15b2c1742058ebc1b8babc41
Waheed Afolabi <waheed.remote247@gmail.com>
2022-12-06 18:53:52 +0100

    Added functionality to present the mail composer

--- MailKomposer/MailKomposer/ViewController.swift
+++ MailKomposer/MailKomposer/ViewController.swift
@@ -6,10 +6,31 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    
+    // MARK: - Properties
+    
+    let messageViewCOntroller = MFMailComposeViewController()
 
+    // MARK: - Object Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageViewCOntroller.mailComposeDelegate = self
+    }
+    
+    // MARK: - Action
+    
+    @IBAction func sendAMessageDidTap(_ sender: UIButton) {
+        if !MFMailComposeViewController.canSendMail() {
+            print("Mail services are not available")
+        } else {
+            messageViewCOntroller.setToRecipients(["sampleemail@address.com"])
+            messageViewCOntroller.setSubject("Mail Komposer")
+            messageViewCOntroller.setMessageBody("Hello from Mail Komposer!", isHTML: false)
+            self.present(messageViewCOntroller, animated: true)
+        }
     }
 }
